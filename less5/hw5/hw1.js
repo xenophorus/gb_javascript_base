87aa7afbbb495af764c2737444380558d0469d1f
xenophorus <xenophorus@gmail.com>
2020-03-26 23:01:42 +1000

    modal win

--- less5/hw5/hw1.js
+++ less5/hw5/hw1.js
@@ -1,2 +1,13 @@
+'use strict';
 
+let modalWindow = document.getElementById('modalWin');
+
+function showModal() {
+    console.log('clicked');
+    modalWindow.style.display = 'block';
+}
+
+function closeModal() {
+    modalWindow.style.display = 'none';
+}
 
